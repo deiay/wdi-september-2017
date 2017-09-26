48211cb63ecdfd7952b8bcdbb4f7aec18ec81009
NatalieBlack <natalievictoriablack@gmail.com>
2017-09-26 12:04:07 -0400

    in-class code for second half of debugging

---
+++ 12-debugging/notes.rb
@@ -0,0 +1,24 @@
+## Habits for avoiding bugs
+- running your code frequently
+- using IRB to try out bits of code before adding them to your program
+- reading your code
+- slow down
+- start with pseudocode, or just notes on paper
+-
+
+## Techniques for solving bugs
+- read your code
+- read the error message (all of it!)
+  - line number
+  - description of error
+  - stack trace
+  - "did you mean?"
+- printing things to the screen (values of variables)
+- google the error (or stack overflow)
+- talk to someone else
+
+
+
+
+
+
