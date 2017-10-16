8570e04fd22aaa6f188654938fd05f6c7ff38b3c
Bitmaker Labs <bitmakerlabs@Bitmaker-3.local>
2017-10-16 12:12:49 -0400

    [25-migrations-and-validations] Add validations

--- 25-migrations-and-validations/instructor/chinook/app/models/track.rb
+++ 25-migrations-and-validations/instructor/chinook/app/models/track.rb
@@ -3,6 +3,15 @@ class Track < ApplicationRecord
   SHORT = 180000
   LONG  = 360000
 
+  validates :name, :composer, :milliseconds, :bytes, :unit_price, presence: true
+  validates :bytes, :milliseconds, numericality: {
+                                     only_integer: true,
+                                     greater_than: 0
+                                   }
+  validates :unit_price, numericality: { greater_than_or_equal_to: 0.0 }
+
+  validate :name_must_be_titleized
+
   scope :short, -> { shorter_than(SHORT) }
   scope :long, -> { longer_than_or_equal_to(LONG) }
   scope :medium, -> { longer_than_or_equal_to(SHORT).shorter_than(LONG) }
@@ -16,6 +25,22 @@ class Track < ApplicationRecord
     else
       all
     end
+  end
+
+private
+
+  def name_must_be_titleized
+    # Step 0: Make sure there is a name
+    return unless name.present?
+
+    # Step 1: Check that the first char is an uppercase letter
+    first_char = name[0]
+
+    # 'a' != 'a'.upcase  ===> 'a' != 'A'
+    first_char_is_not_upcased = (first_char != first_char.upcase)
+
+    # Step 2: If the first char is not uppercase, add an error
+    errors.add(:name, 'must be capitalized') if first_char_is_not_upcased
   end
 
 end
