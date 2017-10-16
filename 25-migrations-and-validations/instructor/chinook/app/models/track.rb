e1b5abda15baaa349b9bf89be69d24cbb8dc47fb
Bitmaker Labs <bitmakerlabs@Bitmaker-3.local>
2017-10-16 10:43:13 -0400

    [25-migrations-and-validations] Difference between scope and class method

--- 25-migrations-and-validations/instructor/chinook/app/models/track.rb
+++ 25-migrations-and-validations/instructor/chinook/app/models/track.rb
@@ -6,8 +6,16 @@ class Track < ApplicationRecord
   scope :short, -> { shorter_than(SHORT) }
   scope :long, -> { longer_than_or_equal_to(LONG) }
   scope :medium, -> { longer_than_or_equal_to(SHORT).shorter_than(LONG) }
-  scope :shorter_than, -> (milliseconds) { where('milliseconds < ?', milliseconds) }
+  scope :shorter_than, -> (milliseconds) { where('milliseconds < ?', milliseconds) if milliseconds.present? && milliseconds > 0 }
   scope :longer_than_or_equal_to, -> (milliseconds) { where('milliseconds >= ?', milliseconds) }
   scope :starts_with, -> (char) { where('name ILIKE ?', "#{ char }%") }
+
+  def self.shorter_than_method(milliseconds)
+    if milliseconds.present? && milliseconds > 0
+      where('milliseconds < ?', milliseconds)
+    else
+      all
+    end
+  end
 
 end
