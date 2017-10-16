ac1647c225c6948529d63a4621d04aaaf57dca14
Bitmaker Labs <bitmakerlabs@Bitmaker-3.local>
2017-10-16 10:33:29 -0400

    [25-migrations-and-validations] Convert class methods to scopes

--- 25-migrations-and-validations/instructor/chinook/app/models/track.rb
+++ 25-migrations-and-validations/instructor/chinook/app/models/track.rb
@@ -3,28 +3,11 @@ class Track < ApplicationRecord
   SHORT = 180000
   LONG  = 360000
 
-  def self.short
-    shorter_than(SHORT)
-  end
-
-  def self.long
-    longer_than_or_equal_to(LONG)
-  end
-
-  def self.medium
-    longer_than_or_equal_to(SHORT).shorter_than(LONG)
-  end
-
-  def self.shorter_than(milliseconds)
-    where('milliseconds < ?', milliseconds)
-  end
-
-  def self.longer_than_or_equal_to(milliseconds)
-    where('milliseconds >= ?', milliseconds)
-  end
-
-  def self.starts_with(char)
-    where('name ILIKE ?', "#{ char }%")
-  end
+  scope :short, -> { shorter_than(SHORT) }
+  scope :long, -> { longer_than_or_equal_to(LONG) }
+  scope :medium, -> { longer_than_or_equal_to(SHORT).shorter_than(LONG) }
+  scope :shorter_than, -> (milliseconds) { where('milliseconds < ?', milliseconds) }
+  scope :longer_than_or_equal_to, -> (milliseconds) { where('milliseconds >= ?', milliseconds) }
+  scope :starts_with, -> (char) { where('name ILIKE ?', "#{ char }%") }
 
 end
