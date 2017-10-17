7264a25dbf07290dd86d2b104f4c680f6c95cefe
NatalieBlack <natalievictoriablack@gmail.com>
2017-10-17 11:13:42 -0400

    in-class code for first half of 26-associations

--- 26-associations/photogur/app/models/picture.rb
+++ 26-associations/photogur/app/models/picture.rb
@@ -1,2 +1,11 @@
 class Picture < ActiveRecord::Base
+  has_many :comments
+  # there is a picture_id column in the comments table
+  # pic.comments
+  # pic.comments << my_comment
+  # pic.comments.count
+  # pic.comments.where
+  # pic.comments.build
+  # pic.comments.create
+  # etc
 end
