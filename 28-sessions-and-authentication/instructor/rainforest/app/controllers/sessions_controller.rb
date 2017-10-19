731efec9b67c579329418debe839234d639771f5
Bitmaker Labs <bitmakerlabs@Bitmaker-3.local>
2017-10-19 12:06:32 -0400

    [28-sessions-and-authentication] Intro to session hash

--- 28-sessions-and-authentication/instructor/rainforest/app/controllers/sessions_controller.rb
+++ 28-sessions-and-authentication/instructor/rainforest/app/controllers/sessions_controller.rb
@@ -6,6 +6,10 @@ class SessionsController < ApplicationController
     u = User.find_by(email: params[:session][:email])
 
     if u && u.authenticate(params[:session][:password])
+
+      # Put the wristband on the User
+      session[:user_id] = u.id
+
       redirect_to root_url
     else
       render :new
@@ -15,3 +19,8 @@ class SessionsController < ApplicationController
   def destroy
   end
 end
+
+# These 3 magic hashes are always available to you in a Rails Controller
+# params
+# session
+# flash
