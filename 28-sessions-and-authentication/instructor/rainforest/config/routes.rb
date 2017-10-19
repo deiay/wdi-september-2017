7613eb2b956c2d58070bb7cceb6c5e82038ec130
Bitmaker Labs <bitmakerlabs@Bitmaker-3.local>
2017-10-19 11:23:28 -0400

    [28-sessions-and-authentication] Remove useless create view, add restful routes for users

--- 28-sessions-and-authentication/instructor/rainforest/config/routes.rb
+++ 28-sessions-and-authentication/instructor/rainforest/config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  get 'users/create'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  resource :users, only: %i(create new)
   resources :products, only: %i(index)
-
   root 'products#index'
 end
