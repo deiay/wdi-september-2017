87dd08aaff8f114ec0fde47d5f8e6f9678c3c98c
NatalieBlack <natalievictoriablack@gmail.com>
2017-10-26 11:51:28 -0400

    in-class code for second half of how-to-test

--- 31-how-to-test/test_temperature.rb
+++ 31-how-to-test/test_temperature.rb
@@ -4,4 +4,43 @@ require_relative 'temperature.rb'
 
 class TestTemperature < MiniTest::Test
    # tests go in here
+
+   def test_f_to_c
+     # arrange
+     f_temp = 50
+
+     # act
+     c_temp_result = f_to_c(f_temp)
+
+     # assert
+     expected = 10
+     assert_equal(expected, c_temp_result)
+   end
+
+   def test_f_to_c_negative
+     f_temp = -4
+
+     result = f_to_c(f_temp)
+
+     expected = -20
+     assert_equal(expected, result)
+   end
+
+   def test_c_to_f_positive
+     c_temp = 10
+
+     result = c_to_f(c_temp)
+
+     expected = 50
+     assert_equal(expected, result)
+   end
+
+   def test_c_to_f_negative
+     c_temp = -20
+
+     result = c_to_f(c_temp)
+
+     expected = -4
+     assert_equal(expected, result)
+   end
 end
