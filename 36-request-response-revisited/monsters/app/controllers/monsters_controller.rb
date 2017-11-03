21c17e8ca7605516b358ce66e027a63b09051723
NatalieBlack <natalievictoriablack@gmail.com>
2017-11-03 12:32:03 -0400

    in-class code for second half of request-response-revisited

--- 36-request-response-revisited/monsters/app/controllers/monsters_controller.rb
+++ 36-request-response-revisited/monsters/app/controllers/monsters_controller.rb
@@ -1,6 +1,23 @@
 class MonstersController < ApplicationController
   def index
     @monsters = Monster.all
+
+    respond_to do |format|
+      format.html
+      format.text
+      format.json do
+        json_data = @monsters.map {|monster|
+          {id: monster.id,
+           name: monster.name,
+           creepiness: monster.creepiness,
+           home: monster.home}
+        }
+
+        render json: {monsters: json_data, 
+                      time: Time.now,
+                      count: @monsters.count}
+      end
+    end
   end
 end
 
