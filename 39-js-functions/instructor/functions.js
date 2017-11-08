d41b6121d35accd045389dec920aaad87a2f6c37
Mina Mikhail <mina@fightingtheboss.com>
2017-11-08 12:32:03 -0500

    Add in-class code for 39 JS Functions & Callbacks

--- 39-js-functions/instructor/functions.js
+++ 39-js-functions/instructor/functions.js
@@ -2,9 +2,27 @@
 
 // Write a function that takes someone's order
 
+// Function declaration
+function takeOrder(order) {
+  console.log("Received order for " + order);
+  return order;
+}
+
+// Function expression
+var takeOrder = function(order) {
+  console.log("Hello from function expression!");
+};
+
 // Call the function
+takeOrder("burger");
 
+takeOrder();
 
+function sayHello() {
+  console.log('Hello JavaScript!');
+}
+
+sayHello();
 
 // Questions:
 
