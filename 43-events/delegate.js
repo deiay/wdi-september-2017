0b0067b5e9f5914c0db4a5bb1a11a67bdd067b2c
Mina Mikhail <mina@fightingtheboss.com>
2017-11-13 09:12:47 -0500

    Update pre-class code for 43 Events

--- 43-events/delegate.js
+++ 43-events/delegate.js
@@ -1,25 +1,28 @@
 // Global scope
 
-$(document).ready(function() {
+document.addEventListener('DOMContentLoaded', function() {
   // Function scope
 
   var count = 0,
       colors = ['tomato', 'azure', 'green', 'orange', 'purple', 'chartreuse', 'yellow', 'chocolate'];
 
+  var container = document.querySelector('#container');
+  var boxMaker = document.querySelector('#box-maker');
 
-  $('#box-maker').on('click', function() {
+  boxMaker.addEventListener('click', function() {
       var x = Math.ceil(Math.random() * 400),
           y = Math.ceil(Math.random() * 200),
-          c = Math.ceil(Math.random() * 8);
+          c = Math.ceil(Math.random() * 7);
 
-      $('<div class="circle"></div>')
-          .css({
-              top: y,
-              left: x,
-              backgroundColor: colors[c]
-          })
-          .html(count++)
-          .appendTo('#container');
+      var circle = document.createElement('div');
+
+      circle.className = 'circle';
+      circle.style.top = y + 'px';
+      circle.style.left = x + 'px';
+      circle.style.backgroundColor = colors[c];
+      circle.innerHTML = count++;
+
+      container.append(circle);
 
       console.log('link was clicked');
   });
