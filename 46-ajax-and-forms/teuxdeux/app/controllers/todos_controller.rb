68e1193243babc1ac3e0e24d1391f477529c400b
Mina Mikhail <mina@fightingtheboss.com>
2017-11-16 12:46:25 -0500

    Add in-class code for 46 AJAX and Forms

---
+++ 46-ajax-and-forms/teuxdeux/app/controllers/todos_controller.rb
@@ -0,0 +1,30 @@
+class TodosController < ApplicationController
+  def index
+    @todos = Todo.all
+    @todo = Todo.new
+  end
+
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.new
+
+    @todo.content = params[:todo][:content]
+
+    if @todo.save
+      respond_to do |format|
+        format.html do
+          if request.xhr?
+            render partial: 'todo', locals: { todo: @todo }
+          else
+            redirect_to root_url
+          end
+        end
+
+        format.json { render json: @todo }
+      end
+    end
+  end
+end
