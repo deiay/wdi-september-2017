b4edfa89b0d0a6008c96badd9b700e410f2b2c71
David Aideyan <david@wealthsimple.com>
2019-04-20 21:53:32 -0400

    added exercise3

---
+++ 06-programming-fundamentals-part-1/student/exercise2.rb
@@ -0,0 +1,21 @@
+#Question 1
+
+meal_cost = 55
+
+tip_amount = meal_cost * 0.20
+
+#Question 2
+
+puts "You should tip $" + tip_amount.to_i.to_s + " dollars for a $#{meal_cost} meal"
+
+#Question 3
+
+num_1 = 45628
+num_2 = 7839
+puts "#{num_1} * #{num_2} = #{num_1 * num_2}"
+
+#Quetion 4
+
+#I expect that this will be true
+
+puts (10 < 20 && 30 < 20) || !(10 == 11)
