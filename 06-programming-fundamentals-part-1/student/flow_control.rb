399d5b84b33952ad143a88eed6b0f0d25988e426
David Aideyan <david@wealthsimple.com>
2019-04-20 21:39:10 -0400

    added completed exercise1

---
+++ 06-programming-fundamentals-part-1/student/flow_control.rb
@@ -0,0 +1,16 @@
+count = 100
+if count == 10
+  puts "count is equal to 10"
+elsif count < 10
+  puts "count is less than 10"
+else
+  puts "count is not equal to 10. It is equal to #{count}"
+end
+
+puts "end of program"
+
+
+puts "count is greater than 10" if count > 10
+
+
+puts "count is greater than 10" unless count <= 10
---
+++ 07-programming-fundamentals-methods/student/methods.rb
@@ -0,0 +1,19 @@
+number_one = 10
+number_two = 20
+
+result = number_one + number_two
+
+def sum(first_number,second_number)
+  # Body of the method
+  #first_number = 10
+  #second_number = 20
+  #puts "#{first_number}, #{second_number}"
+  result = first_number + second_number
+  result
+
+
+end
+
+result = sum(10,20)
+
+print result
