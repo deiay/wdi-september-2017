28a88579da7ce86ce37348fe04c00554f2dfc50c
David Aideyan <david@wealthsimple.com>
2019-04-20 22:01:20 -0400

    Ask for age and compute birth year

--- 06-programming-fundamentals-part-1/student/exercise3.rb
+++ 06-programming-fundamentals-part-1/student/exercise3.rb
@@ -1,3 +1,9 @@
 puts "What's your name?"
-name = gets
-puts "Hi #{name}!"
+
+user_name = gets.chomp
+
+puts "Hi #{user_name}! How old are you?"
+user_age = gets.chomp.to_i
+birth_year = "#{2019 - user_age} or #{2019 - user_age - 1}"
+
+puts "#{user_age}! That means you must have been born in #{birth_year}"
